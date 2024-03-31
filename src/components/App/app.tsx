8d57602ffb90d0887dde15e2bcfd74addc8e4743
Justus Lambrecht <justuslambrecht@icloud.com>
2024-03-31 16:22:26 +0200

    inital

---
+++ src/components/App/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './app.css';
+import "../../fonts/PressStart2P-Regular.ttf";
+
+class App extends React.Component {
+  render() {
+    return <div>Pokedex</div>
+
+  }
+}
+
+export default App;
