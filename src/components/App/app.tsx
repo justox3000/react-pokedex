48cdc3976a8a4dcd52b6a2d59f81551c4d3b34eb
Justus Lambrecht <justuslambrecht@icloud.com>
2024-03-31 16:36:04 +0200

    container created

--- src/components/App/app.tsx
+++ src/components/App/app.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Pokedex from "../pokedex/pokedex";
 import './app.css';
 import "../../fonts/PressStart2P-Regular.ttf";
 
 class App extends React.Component {
   render() {
-    return <div>Pokedex</div>
+    return <div className="app">
+      <h1>Pokedex</h1>
+      <Pokedex />
+      </div>
 
   }
 }
